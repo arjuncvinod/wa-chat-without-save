a212d2f6766ba5b6efe29a0a3c4de2c412b3497c
arjuncvinod <arjuncvinod007@gmail.com>
2023-08-17 23:21:40 +0530

    textbox added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import './App.css';
 
 function App() {
+  let no ="+918304069399"
   return (
-   <h1>hello</h1>
+    <main>
+      <input type="text" />
+      <a href={`https://wa.me/${no}`}>
+        <button>Chat</button>
+      </a>
+    </main>
   );
 }
 
