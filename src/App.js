0434f615d81c1a0d6daf7234b98b1400abec87fa
arjuncvinod <arjuncvinod007@gmail.com>
2023-08-18 00:22:32 +0530

    app.js updated

--- src/App.js
+++ src/App.js
@@ -8,7 +8,12 @@ const [no,setNo]=useState("")
   }
   return (
     <main>
+    <div className='header'>
+     <img src="wa.png" />
+      <h1> Chat without saving the contact</h1>
+    </div>
     <div className='container'>
+    
       <input type="text" onChange={handleChange} placeholder='Enter the number'/>
       <a href={`https://wa.me/+91${no}`}>
         <button>Chat</button>
