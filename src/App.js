d0d98ac1120e8903639421961bc611a72618ff37
arjuncvinod <arjuncvinod007@gmail.com>
2023-08-20 00:04:52 +0530

    copyright section added

--- src/App.js
+++ src/App.js
@@ -14,19 +14,28 @@ const [no,setNo]=useState("")
   }
   return (
     <main>
-    <div className='header'>
-     <img src="wa.png" alt=''/>
-      <h1> Chat without saving the number</h1>
-    </div>
-    <div className='container'>
-      <div>
-      
-      <input type="text" onChange={setCountry} value={countryCode}/>
-      <input type="text" onChange={handleChange} placeholder='Enter the number'/>
+      <div className="header">
+        <img src="wa.png" alt="" />
+        <h1> Chat without saving the number</h1>
       </div>
-      <a href={`https://wa.me/${countryCode}${no}`}>
-        <button>Chat</button>
-      </a>
+      <div className="container">
+        <div>
+          <input type="text" onChange={setCountry} value={countryCode} />
+          <input
+            type="text"
+            onChange={handleChange}
+            placeholder="Enter the number"
+          />
+        </div>
+        <a href={`https://wa.me/${countryCode}${no}`}>
+          <button>Chat</button>
+        </a>
+      </div>
+      <div class="copyright">
+        <p class="made">Made with 💚 by <a href='https://arjuncvinod.netlify.app'>Arjun</a></p>
+        <p>
+          Copyright &copy; 2023
+        </p>
       </div>
     </main>
   );
